d94511f683f22f4b313967236c04595fa85e006a
Rashed967 <rashedhasan967.com>
2023-06-29 21:10:45 +0600

    just created repo:

---
+++ src/routes/Routes.jsx
@@ -0,0 +1,19 @@
+
+import { createBrowserRouter } from 'react-router-dom';
+import Main from '../layout/Main';
+import Home from '../pages/Home/Home/Home';
+
+const Routes = createBrowserRouter([
+    {
+      path: "/",
+      element: <Main></Main>,
+      children : [
+        {
+            path : "/",
+            element : <Home></Home>
+        }
+      ]
+    },
+  ]);
+
+export default Routes;
