6b9b81d19d83a26dbdbb941b3bf816c6457acb61
Rashed967 <rashedhasan967.com>
2023-06-30 21:59:11 +0600

    banner part done

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -5,81 +5,90 @@ import bannerImg from '../../assets/banner-img.jpg'
 const Banner = () => {
     return (
         <div>
-           <Grid container sx={{background : '#A9D6E5'}} >
-            <Grid item xs={12} md={5}
-            sx={{
-                display : 'flex',
-                alignItems : 'center',
-                justifyContent : 'center',
-                position : 'relative',
-                
-                '& img' : {
-                    // width : '50%',
-                    height : '50%',
-                    zIndex : 2,
-                    
-                    
-                }
-            }}
-            >
-                <img src={bannerImg} alt="" />
-                <Box sx={{
-                    background : '#012A4A',
-                    width : 340,
-                    height : 340,
-                    borderRadius : 3,
-                    position : 'absolute',
-                    zIndex : 1,
-                    ml : 5,
-                    mt : 6,
-                    mr : '7px'
-                    // my : -15
-                }}></Box>
+            <Grid container sx={{ background: '#A9D6E5', py : '100px'}} >
+                <Grid item xs={12} md={6}
+                    sx={{
+                        display: 'flex',
+                        alignItems: 'center',
+                        justifyContent: 'center',
+
+                    }}
+                >
+
+                    <Box
+                        sx={{
+                            position: 'relative',
+                            width: '60%',
+                            paddingTop: '60%',
+                            borderRadius: '50%',
+                            background : 'red',
+                            overflow: 'hidden',
+                        }}
+                    >
+                        <Box
+                            component="img"
+                            src={bannerImg}
+                            alt="Your Image"
+                            sx={{
+                                position: 'absolute',
+                                top: '0',
+                                left: '0',
+                                width: '100%',
+                                height: '100%',
+                                objectFit: 'cover',
+                            }}
+                        />
+                    </Box>
+
+
+                </Grid>
+
+                <Grid item xs={12} md={6} sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center', px: 5, }}>
+                    <Box sx={{ position: 'relative', my: '70px' }}>
+
+                        <Box sx={{
+                            width: '5px',
+                            height: '120%',
+                            background: '#012A4A',
+                            // transform : 'rotate(90deg)',
+                            // transformOrigin : 'top left',
+                            position: 'absolute',
+                            left: '-20px',
+                            top: '-10px',
+
+                        }}>
+
+                            <Box sx={{
+                                width: '15px',
+                                height: '15px',
+                                borderRadius: '50%',
+                                background: '#012A4A',
+                                position: 'absolute',
+                                left: '-5px',
+                                top: '-5px'
+                            }}></Box>
+
+                            <Box sx={{
+                                width: '15px',
+                                height: '15px',
+                                borderRadius: '50%',
+                                background: '#012A4A',
+                                position: 'absolute',
+                                right: '-5px',
+                                bottom: '-5px'
+                            }}></Box>
+
+                        </Box>
+
+                        <Typography variant='h2' component="h1" fontWeight={700} sx={{ color: '#012A4A' }}>ABDUR RAHMAN</Typography>
+                        <Typography variant='body1' sx={{ color: '#012A4A' }}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur deleniti ipsam nulla? Deserunt, voluptates ratione.</Typography>
+                        <Button sx={{ mt: 2, background: '#012A4A', color: 'white', py: 1, px: 2 }} variant="contained"><Typography variant='button' fontWeight={400}>Download CV</Typography></Button>
+
+                    </Box>
+
+                </Grid >
+
             </Grid>
-
-            <Grid item xs={12} md={7} sx={{display : 'flex', alignItems : 'center', justifyContent : 'center', px : 5, }}>
-                <Box sx={{position : 'relative', my : '70px'}}>
-                <Typography variant='h2' fontWeight={700} sx={{color : '#012A4A'}}>ABDUR RAHMAN</Typography>
-                <Typography variant='body1' sx={{color : '#012A4A'}}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur deleniti ipsam nulla? Deserunt, voluptates ratione.</Typography>
-                <Button sx={{mt : 2, background : '#012A4A', color : 'white', py : 1, px : 2}} variant="contained"><Typography variant='button' fontWeight={400}>Download CV</Typography></Button>
-                <Box sx={{
-                    width : '230px',
-                    height : '5px',
-                    background : '#012A4A',
-                    transform : 'rotate(90deg)',
-                    transformOrigin : 'top left',
-                    position : 'absolute',
-                    left : '-20px',
-                    top : '-10px',
-                
-                }}>
-
-                    <Box sx={{
-                        width: '15px',
-                        height : '15px',
-                        borderRadius : '50%',
-                        background : '#012A4A',
-                        position : 'absolute',
-                        left : '-5px',
-                        top : '-5px'
-                    }}></Box>
-
-                    <Box sx={{
-                        width: '15px',
-                        height : '15px',
-                        borderRadius : '50%',
-                        background : '#012A4A',
-                        position : 'absolute',
-                        right : '-5px',
-                        top : '-5px'
-                    }}></Box>
-
-                </Box>
-                </Box>
-                
-            </Grid >
-
-           </Grid>
         </div>
     );
 };
