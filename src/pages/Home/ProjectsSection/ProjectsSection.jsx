42c3754d56fb6da1a1223665922faeb20e611e3b
Rashed967 <rashedhasan967.com>
2023-07-01 21:40:55 +0600

    added projects section

--- src/pages/Home/ProjectsSection/ProjectsSection.jsx
+++ src/pages/Home/ProjectsSection/ProjectsSection.jsx
@@ -1,4 +1,5 @@
-import { Box, Grid, Typography } from "@mui/material";
+import { Height } from "@mui/icons-material";
+import { Box, Button, Grid, Typography } from "@mui/material";
 import { useEffect, useState } from "react";
 
 
@@ -17,74 +18,74 @@ const ProjectsSection = () => {
 
 
     return (
-        <Grid container sx={{ background: '#A9D6E5' , overflow : 'hidden'}}>
-            <Box sx={{ width: '100%', display: 'flex', justifyContent: 'center' }}><Typography variant="h4" fontWeight="500" color="#012A4A" my="15px">Projects Gallery</Typography></Box>
-
-            <Box sx={{ display: 'flex',
-            // width : '100%',
-            justifyContent: 'center',
-            gap : '20px',
-            flexWrap :  'wrap',
-            '@media (max-width: 576px)': {
-                width : '90%',
-                '& > *': {
-                  flex: '1 1 100%',
-                  
-                },
-              },
-            '@media (min-width: 576px) and (max-width : 767px)': {
-                width : '70%',
-                '& > *': {
-                  flex: '1 1 100%',
-                  
-                },
-              },
-              '@media (min-width: 768px) and (max-width: 1279px)': {
-                '& > *': {
-                  flex: '1 1 50%',
-                },
-              },
-              '@media (min-width: 1280px)': {
-                '& > *': {
-                  flex: '1 1 33.33%',
-                },
-              },
-            
-            }}>
-          {
-            projects.map(pr => <Box key={pr._id}
+        <Grid container sx={{ background: '#A9D6E5', overflow: 'hidden' }}>
+        <Box sx={{ width: '100%', display: 'flex', justifyContent: 'center' }}>
+          <Typography variant="h4" fontWeight="500" color="#012A4A" my="15px">
+            Projects Gallery
+          </Typography>
+        </Box>
+  
+        <Box width="100%">
+          <Grid container spacing={2}>
+           {
+            projects.map(pr =>  <Grid key={pr._id} item xs={12} sm={6} md={4} sx={{
+                '& > *' : {
+                    m : '10px'
+                }
+            }}>       
+              <Box
                 sx={{
-                  height: '300px',
-                  background: 'red',
-                  
+                  background: 'white',
+                  height: 'auto',
+                  pb : '25px',
+                borderRadius : '7px',
+                
+                  '& img' : {
+                    '@media (max-width : 600px)' : {
+                        width : '100%'
+                    },
+                    '@media (min-width : 601px) and (max-width : 900px)' : {
+                        width : '100%'
+                    },
+                    '@media (min-width : 901px)' : {
+                        width : '100%'
+                    } 
+                  }  
                 }}
               >
-               
-                <Box sx={{
-                    width : '100%',
-                    '& img' : {
-                        width : '30%',
-                        '@media (max-width: 567px)': {
-                            width : '70%'
-                        },
-                        '@media (min-width : 568px) and (max-width: 867px)' : {
-                            width : '50%'
-                        },
-                        '@media (min-width : 868px) and (max-width : 991px)' : {
-                            width : '30%'
-                        }
+                <img src={pr.image} alt="" />
+                <Box><Typography align="center" variant="h6">{pr.name}</Typography></Box>
+
+              <Box m="15px">
+                <Typography variant="subtitle1" fontWeight="600">Short description:</Typography>
+                <Typography>{pr.description.slice(0, 80)}</Typography>
+              </Box>
+              <Box  m="25px" sx={{display : 'flex', justifyContent : 'center'}}>
+                <Button variant="contained" sx={{
+                    background : "#012A4A",
+                    fontWeight : '600',
+                    border : '3px solid #012A4A',
+                    '&:hover' : {
+                        background : 'white',
+                        border : '3px solid #012A4A',
+                        color : '#012A4A',
+                        fontWeight : '600'
                     }
+                    
                 }}>
-                    <img src={pr.image} alt="" />
-                </Box>
-                <Box><Typography>{pr.name}</Typography></Box>
-              </Box>)
-          }      
-
-      
-
-    </Box>
-        </Grid>
+                    <Typography variant="body1" fontWeight={300}>See full details</Typography>
+                </Button>
+              </Box>
+              </Box>
+              
+            </Grid>
+            )
+           }
+           
+     
+          </Grid>
+        </Box>
+      </Grid>
     );
 };
 
