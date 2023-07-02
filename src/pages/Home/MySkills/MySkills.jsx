bd8902b3de4a8ea93eb66b65f1dd67357e4b64da
Rashed967 <rashedhasan967.com>
2023-07-02 12:53:56 +0600

    skills part doen

--- src/pages/Home/MySkills/MySkills.jsx
+++ src/pages/Home/MySkills/MySkills.jsx
@@ -1,62 +1,125 @@
 import { Box, Grid, LinearProgress, Typography } from '@mui/material';
-import React from 'react';
+
+
 
 const MySkills = () => {
+
+    const skills = [
+        {
+            "name" : "HTML",
+            "ratting" : "95%",
+            "id" : 1
+        },
+        {
+            "name" : "CSS",
+            "ratting" : "85%",
+            "id" : 2
+        },
+        {
+            "name" : "Javascript",
+            "ratting" : "75%",
+            "id" : 3
+        },
+        {
+            "name" : "React",
+            "ratting" : "85%",
+            "id" : 4
+        },
+        {
+            "name" : "Mongodb",
+            "ratting" : "75%",
+            "id" : 5
+        },
+        {
+            "name" : "Tailwind",
+            "ratting" : "85%",
+            "id" : 6
+        },
+        {
+            "name" : "Node js",
+            "ratting" : "50%",
+            "id" : 7
+        },
+        {
+            "name" : "Git & GitHub",
+            "ratting" : "80%",
+            "id" : 8
+        },
+
+    ]
     return (
-        <div>
+        <div >
             <Box sx={{
-                width : '100%',
-                background : '#014F86',
-                py : '40px'
+                width: '100%',
+                background: '#014F86',
+                py: '40px',
+                // px : '3%',
+                // gap : '10px',
+                display : 'flex',
+                justifyContent : 'center'
             }}>
-                <Box sx={{
-                    width : '100%',           
+               <Box sx={{width : '95%'}}>
+               <Box sx={{
+                    width: '100%',
                 }}>
                     <Typography variant='h4' align='center' fontWeight={500} color="white">My skills</Typography>
                 </Box>
 
                 <Box sx={{
-                    width : "100%",
-                    mt : '40px'
+                    width: "100%",
+                    mt: '40px'
                 }}>
                     <Grid container spacing={2}>
-                        <Grid item xs={12} md={6} >
-                            <Box sx={{
-                                background:"white",
-                                height : "100px"
-                            }}>
-                                <Box>
-                                    <Typography variant='h6' color="blue">Html</Typography>
-                                </Box>
-                                <Box>
-                                    <Box sx={{width: "100%"}}>
-                                        <Typography>80%</Typography>
-                                        <LinearProgress variant="determinate"
-                                value={80}
-                                sx={{
-                                    height : '15px'
-                                }}
-                                /> 
+                         
+                        {
+                            skills.map(skill => <Grid key={skill.id} item xs={12} md={6} >
+                                <Box sx={{
+                                    
+                                    height: "100px",
+                                    width : '95%'
+                                }}>
+                                    <Box>
+                                        <Typography variant='h6' color="white" mb="5px" ml="5px">{skill.name}</Typography>
+                                    </Box>
+                               
+                                    <Box sx={{
+                                        width: '100%',
+                                        background: 'white',
+                                        height: '32px',
+                                        borderRadius: '15px',
+                                        display: 'flex',
+                                        justifyContent: 'start',
+                                        alignItems: 'center',
+                                        pl: '5px',
+                                    }}>
+    
+                                        <Box sx={{
+                                            width: `${skill.ratting}`,
+                                            height: '24px',
+                                            background: '#012A4A',
+                                            borderRadius: "15px",
+                                            
+                                        }}>
+                                            <Typography color="white"
+                                            align='center'
+                                            fontWeight={600}
+                                            >
+                                                {skill.ratting}
+                                            </Typography>
+                                        </Box>
                                     </Box>
                                 
                                 </Box>
-                                 </Box>
-                        </Grid>
-                        <Grid item xs={12} md={6}>
-                            <Box sx={{
-                                background:"red",
-                                height : "100px"
-                            }}> </Box>
-                        </Grid>
-                        <Grid item xs={12} md={6}>
-                            <Box sx={{
-                                background:"red",
-                                height : "100px"
-                            }}> </Box>
-                        </Grid>
+                            </Grid>)
+                        }
+                       
+                        
+                        
+
                     </Grid>
 
                 </Box>
+               </Box>
 
             </Box>
         </div>
