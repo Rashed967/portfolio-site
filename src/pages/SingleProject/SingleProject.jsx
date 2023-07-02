493bfaf4f77ca09b837d1ae71371a9f3ba413098
Rashed967 <rashedhasan967.com>
2023-07-02 21:24:38 +0600

    added some code :

--- src/pages/SingleProject/SingleProject.jsx
+++ src/pages/SingleProject/SingleProject.jsx
@@ -1,4 +1,5 @@
-import { Box, List, ListItem, ListItemText, Typography } from '@mui/material';
+import { FiberManualRecord } from '@mui/icons-material';
+import { Box, List, ListItem, ListItemIcon, ListItemText, Typography } from '@mui/material';
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
@@ -19,39 +20,43 @@ const SingleProject = () => {
     console.log(project)
     return (
         <div>
-             <Box sx={{ width: '100%', background : "#A9D6E5"}}>
+             <Box sx={{ width: '100%', background : "#A9D6E5",}}>
                 <Typography variant='h4'fontWeight={600} py="30px" color="#012A4A" align="center">Projects Details</Typography>
             </Box>
             <Box sx={{widht : "100%"}}>
                 <img width="100%" src={project.image} alt="" />
             </Box>
-            <Box sx={{width : "100%", background :"#014F86", display : "flex", justifyContent : "center", alginItmes :"center"}}>
+            <Box sx={{width : "100%", background :"#014F86", display : "flex", justifyContent : "center", alginItmes :"center", py : "40px"}}>
                 <Box sx={{  width : "90%"}}>
-                    <Box>
+                    <Box >
                     <Typography variant='subtitle1' color="white">Project description:</Typography>
                     <Typography color="white" variant='body2' fontWeight={300}>{project.description}</Typography>
                     </Box>
-                    <Box>
+                    <Box  sx={{mt : "15px"}}>
                     <Typography variant='subtitle1' color="white">Project Features:</Typography>
-                    <List>
+                    <List disablePadding="true" dense="true">
                         {
-                            project.features?.map(feature => <ListItem key={feature}>
-                                <ListItemText><Typography variant='body2' color="white" fontWeight={300}>{feature}</Typography></ListItemText>
+                            project.features?.map(feature => <ListItem sx={{p : "0px"}} key={feature}>
+                               <ListItemIcon ><FiberManualRecord sx={{color : 'white', width : "12px", pr : "5px"}}></FiberManualRecord></ListItemIcon>
+                                <ListItemText sx={{py : -1}}>
+                                
+                                    <Typography variant='body2' color="white" fontWeight={300}>{feature}</Typography></ListItemText>
                             </ListItem>)
                         }
                     </List>
                 </Box>
 
-                <Box>
+                <Box  sx={{mt : "15px"}}>
                     <Typography variant='subtitle1' color="white">Project duration:</Typography>
                     <Typography color="white" variant='body2' fontWeight={300}>{project.duration}</Typography>
                 </Box>
                 
-                <Box>
+                <Box  sx={{mt : "15px"}}>
                     <Typography variant='subtitle1' color="white">Used technologies:</Typography>
                     <List>
                         {
-                            project.technologis?.map(technology => <ListItem key={technology}>
+                            project.technologis?.map(technology => <ListItem sx={{m : "0px", p : "0px"}} key={technology}>
+                                
                                 <ListItemText><Typography variant='body2' color="white" fontWeight={300}>{technology}</Typography></ListItemText>
                             </ListItem>)
                         }
@@ -59,7 +64,7 @@ const SingleProject = () => {
                 </Box>
 
 
-                <Box>
+                <Box  sx={{mt : "15px"}}>
                     <Typography variant='subtitle1' color="white">Project Challanges:</Typography>
                     <Typography color="white" variant='body2' fontWeight={300}>{project.challanges}</Typography>
                 </Box>
